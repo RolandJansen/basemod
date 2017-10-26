1c0db00ce641ee52cc3b9a64d6f3c75598fc3203
Roland Jansen <jansen@stereokrauts.com>
2017-10-26 17:16:30 +0200

    Added open-iconic icon font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'open-iconic/font/css/open-iconic-bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
