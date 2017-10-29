e81813bc4a8cf1116b48b0de401da32dd618316f
Roland Jansen <jansen@stereokrauts.com>
2017-10-29 06:06:40 +0100

    Added linting, redux and some structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopNav from './containers/TopNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopNav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/parts/TopNav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+/**
+ *
+ */
+class TopNav extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default TopNav;
